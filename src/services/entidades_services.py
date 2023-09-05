d57b8dd85a65fb23c4923aed50b9cbebd789e600
Daniel Miramontes <98139576+DanMiramontes@users.noreply.github.com>
2023-09-05 13:42:03 +0000

    refactor

--- src/services/entidades_services.py
+++ src/services/entidades_services.py
@@ -1,23 +1,11 @@
-import spacy
+from src.model.entidades_model import reconocer_entidad
 
 class entidadesServices():
     @classmethod
     def entidades_texto(cls,texto):
-        
         resultado = []
-
         for frase in texto:
             entidades= reconocer_entidad(frase)
             resultado.append({"oracion":frase, "entidades": entidades})
         return resultado;
 
-def reconocer_entidad(frase):
-    nlp = spacy.load('es_core_news_sm')
-    documento = nlp(frase)
-    for entidad in documento.ents:
-        entidad = {
-            entidad.text : entidad.label_
-
-        }
-        return entidad
-   
